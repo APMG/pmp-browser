956a77d976e9a015955c3b54d45607b8d181287a
Peter Karman <pkarman@mpr.org>
2014-03-13 16:37:22 -0500

    add img to search results, tweek pager icons

--- pmp-browser.js
+++ pmp-browser.js
@@ -24,14 +24,16 @@ PMPB.search = function(apiUrl, resultFormatter) {
             total: parseInt(res.total),
             limit: parseInt(res.query.limit),
             index: parseInt(res.query.offset || 0),
-            first: '|<',
-            last:  '>|',
+            first: '|&#171;',
+            prev:  '&#171;',
+            last:  '&#187;|',
+            next:  '&#187;',
             url:   function(idx) {
                 var u = baseUri + '&offset='+(idx*res.query.limit);
                 return u;
             }
         }; 
-        console.log(pagerSettings);
+        //console.log(pagerSettings);
         $('#pager').wPaginate(pagerSettings);
         $.each(res.results, function(idx,r) {
             //console.log(r);
@@ -44,7 +46,9 @@ PMPB.formatResult = function(r) {
     //console.log(r);
     if (!r) return '';
     var url = r.href;
+    var thumbnail = PMPB.findImage(r);
     var d = '<div class="result">';
+       d += '<div class="thumb"><img src="'+thumbnail+'" /></div>';
        d += '<div class="title">';
        d += '<a class="title" href="?doc='+encodeURIComponent(url)+'">';
        d += r.attributes.title;
@@ -63,3 +67,19 @@ PMPB.formatResult = function(r) {
        d += '</div>';
     return d;
 }
+
+PMPB.findImage = function(r) {
+    var resImg = 'http://publicmediaplatform.org/wp-content/uploads/logo.png';
+    $.each(r.items, function(idx, item) {
+        if (item.links.profile[0].href.match(/\/image/)) {
+            $.each(item.links.enclosure, function(idx2, img) {
+                if (img.meta.crop == 'primary') {
+                    resImg = img.href;
+                }
+            });
+        }
+    });
+    return resImg;
+}
+
+                
